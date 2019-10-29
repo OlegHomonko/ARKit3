fc69c456fed2a9f505f81f72a6e0be040586db00
oleg.gomonko <oleg.gomonko@avrspot.com>
2019-10-29 11:03:47 +0200

    init

---
+++ Assets/Scripts/ARCreatePrefab.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ARCreatePrefab : MonoBehaviour
+{
+    [SerializeField] private GameObject environmentPrefab;
+    [SerializeField] private ARRaycastManager arRayManager;
+
+    private void Update()
+    {
+        if (Input.touchCount == 0) return;
+
+        var touch = Input.GetTouch(0);
+        var hitList = new List<ARRaycastHit>();
+
+        if (!arRayManager.Raycast(touch.position, hitList,
+            UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon)) return;
+        var obj = Instantiate(environmentPrefab);
+        obj.transform.position = hitList[0].pose.position;
+    }
+}
